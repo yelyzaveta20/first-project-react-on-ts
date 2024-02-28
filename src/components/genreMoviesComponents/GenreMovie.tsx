3dd3b9382f18808bcdec4ee7d4958ccdfaaaf5dd
Yelyzaveta Smetana <yelyzaveta.smetana@hneu.net>
2024-02-28 22:54:52 +0200

    version

--- src/components/genreMoviesComponents/GenreMovie.tsx
+++ src/components/genreMoviesComponents/GenreMovie.tsx
@@ -1,18 +1,17 @@
-import React, {FC, useEffect, useState} from 'react';
-import {IGenre, IMovie} from "../../interfaces/movisInterface";
-import {useLocation, useNavigate} from "react-router-dom";
+import React, {FC} from 'react';
+import {IMovie} from "../../interfaces/movisInterface";
+import { useNavigate} from "react-router-dom";
 import {poster} from "../../constans/urls";
 import {Rating} from "@mui/material";
 import css from './GenreMovie.module.css'
-import {movieService} from "../../services/movieService";
-import {log} from "node:util";
+
 interface IProps{
     movie:IMovie
 }
 const GenreMovie:FC<IProps> = ({movie}) => {
     // const [genreMovie, setGenreMovie] = useState<IGenre[]>([])
     let navigate = useNavigate();
-    const {genre_ids, title, poster_path, vote_average}=movie
+    const { title, poster_path, vote_average}=movie
     const posterurl=`${poster}${poster_path}`
 
     const handle = () => {
